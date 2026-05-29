776c0c80236623c5a6ae363408db7c414997554c
starlight-web3 <mossei@nate.com>
2026-05-29 18:49:26 +0800

    s

--- next.config.ts
+++ next.config.ts
@@ -9,18 +9,26 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**", // 해당 도메인의 모든 경로 허용
       },
       {
         protocol: "https",
         hostname: "*.cafe24.com",
+        port: "",
+        pathname: "/**", // 해당 도메인의 모든 경로 허용
       },
       {
         protocol: "https",
         hostname: "*.namu.wiki",
+        port: "",
+        pathname: "/**", // 해당 도메인의 모든 경로 허용
       },
       {
         protocol: "https",
         hostname: "image.8dogam.com",
+        port: "",
+        pathname: "/**", // 해당 도메인의 모든 경로 허용
       },
     ],
   },
